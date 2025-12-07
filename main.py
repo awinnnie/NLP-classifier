39fa1d7272947bc7125a227f2cf8f7a50b3a8a4e
awinnnie <114687525+awinnnie@users.noreply.github.com>
2025-12-07 21:22:17 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,20 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import pickle
 import joblib
+import pandas as pd
+import json
 
 model = joblib.load("Models/tuned/svm.pkl")
 app = FastAPI()
+
+data = []
+with open("Data\\News_Category_Dataset_v3.json", "r", encoding="utf-8") as f:
+    for line in f:
+        if line.strip():
+            data.append(json.loads(line))
+
+df = pd.DataFrame(data)
+df = df[['headline', 'category']].reset_index(drop=True)
 
 class InputText(BaseModel):
     text: str
@@ -12,9 +23,75 @@ class InputText(BaseModel):
 @app.post("/predict")
 def predict(data: InputText):
     pred = model.predict([data.text])[0]  # wrap in list
-    return {"prediction": int(pred)} 
+    return {"prediction": pred} 
 
+# 1 get count of category
 
+class CategoryRequest(BaseModel):
+    category: str
+
+@app.post("/count_category")
+def count_category(req: CategoryRequest):
+    cat = req.category.strip().upper()
+    count = df[df["category"] == cat].shape[0]
+    return count
+
+# 2 ten headlines from category
+
+@app.post("/ten_from_category")
+def top_10_category(req: CategoryRequest):
+    cat = req.category.strip().upper()
+    subset = df[df["category"] == cat].reset_index()
+    top10 = []
+    for _, row in subset.head(10).iterrows():
+        top10.append({"id": int(row["index"]), "headline": row["headline"]})
+    return {"category": cat, "headlines": top10}
+
+# add headlines
+@app.post("/add_headline")
+def add_headline(req: InputText):
+    global df
+    pred = model.predict([req.text])[0]
+
+    new_row = {
+        "category": pred,
+        "headline": req.text
+    }
+    
+    df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
+    df.to_json("Data/News_Category_Dataset_v3.json", orient="records", lines=True)
+    
+    return {
+        "message": "Headline added",
+        "id": len(df) - 1,
+        "headline": req.text,
+        "predicted_category": pred
+    }
+    
+# update headline (by id and new headline)
+class UpdateHeadlineRequest(BaseModel):
+    id: int
+    text: str
+    
+@app.put("/update_headline")
+def update_headline(req: UpdateHeadlineRequest):
+    global df
+    if req.id < 0 or req.id >= len(df):
+        return {"error": "Invalid ID"}
+
+    pred = model.predict([req.text])[0]
+
+    df.at[req.id, "headline"] = req.text
+    df.at[req.id, "category"] = pred
+
+    df.to_json("Data/News_Category_Dataset_v3.json", orient="records", lines=True)
+
+    return {
+        "message": "Headline updated",
+        "id": req.id,
+        "new_headline": req.text,
+        "new_category": pred
+    }
 
 # app = FastAPI() 
 # @app.get("/") 
