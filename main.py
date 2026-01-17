70e2885c97d18e0801c1184275b792564d1cafd5
awinnnie <114687525+awinnnie@users.noreply.github.com>
2026-01-17 22:22:48 +0400

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from routers.prediction import router as prediction_router
-from routers.data_analysis import router as data_router
-
-app = FastAPI()
-
-app.include_router(prediction_router, tags=["Prediction"])
-app.include_router(data_router, tags=["Data Analysis"])
